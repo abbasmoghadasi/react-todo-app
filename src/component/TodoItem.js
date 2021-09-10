e3a27d6e2cc91c4c1b325b924398465031aed27f
Abbas Moghadasi <abm@schaeffer-ag.de>
2021-09-10 21:56:25 +0430

    add Header as a functional Component

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-// class TodoItem extends React.Component {
-//   render() {
-//     return <li key={this.props.todo.key}>{this.props.todo.title}</li>;
-//   }
-// }
-
-function TodoItem(props) {
-  return <li>{props.todo.title}</li>;
+class TodoItem extends React.Component {
+  render() {
+    return <li key={this.props.todo.key}>{this.props.todo.title}</li>;
+  }
 }
+
+// function TodoItem(props) {
+//   return <li>{props.todo.title}</li>;
+// }
 
 export default TodoItem;
