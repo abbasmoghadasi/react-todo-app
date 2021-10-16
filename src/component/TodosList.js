6a61951c85b436eea7383f84dcb3f77c41a78bb5
Abbas Moghadasi <abm@schaeffer-ag.de>
2021-10-16 14:37:53 +0330

    Using prevState

--- src/component/TodosList.js
+++ src/component/TodosList.js
@@ -6,7 +6,6 @@ class TodosList extends React.Component {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          //   <li key={todo.id}>{todo.title}</li>
           <TodoItem
             key={todo.id}
             todo={todo}
