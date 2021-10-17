8119e54018319bf9e742c642a7f74e4647875dad
Abbas Moghadasi <abm@schaeffer-ag.de>
2021-10-17 12:43:06 +0330

    define event handler for input text

--- src/component/InputTodo.js
+++ src/component/InputTodo.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
 class InputTodo extends Component {
+  state = {
+    title: "text",
+  };
+  onChange = (e) => {
+    console.log("Hello");
+  };
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo ..." />
+        <input
+          type="text"
+          placeholder="Add Todo ..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     );
